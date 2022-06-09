61df644302921e3cb7cd5c6033955f601442d157
Dante Burnett <93058935+dantedenzel@users.noreply.github.com>
2022-06-08 22:34:42 -0700

    commit to changes?

--- flask_hello/server.py
+++ flask_hello/server.py
@@ -6,8 +6,6 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
-@app.route('/dojo')
-
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
